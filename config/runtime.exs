df8a37784dfdc087471b77fa694be5f56c9223a1
Henrik Jürges <juerges.henrik@gmail.com>
2025-03-02 23:05:45 +0100

    Adjust config

--- config/runtime.exs
+++ config/runtime.exs
@@ -29,6 +29,7 @@ if config_env() == :prod do
       """
 
   config :elixir_rotation, ElixirRotation.Repo,
+    ssl: true,
     database: database_path,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "5")
 
@@ -37,14 +38,14 @@ if config_env() == :prod do
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+  # secret_key_base =
+  #   System.get_env("SECRET_KEY_BASE") ||
+  #     raise """
+  #     environment variable SECRET_KEY_BASE is missing.
+  #     You can generate one by calling: mix phx.gen.secret
+  #     """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+#  host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :elixir_rotation, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
